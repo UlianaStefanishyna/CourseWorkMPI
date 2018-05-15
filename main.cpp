4117c7dbd093e587e75aef25c6d7d44d4dd6cfa5
UlianaStefanishyna <ulanka1996@gmail.com>
2018-05-16 02:56:04 +0300

    half

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <cstring>
-#include "Utils.h"
+#include <vector>
+#include </usr/include/mpich/mpi.h>
+#include <stdio.h>
+#include <iostream>
+#include <sys/resource.h>
+#include <cstdlib>
 
-//const int N = 100;
-//const int P = 5;
-//const int H = N / P;
+using namespace std;
+
+const unsigned int N = 20000;
+const unsigned int P = 4;
+const unsigned int H = N / P;
 
 /**
  * Course Work _ 2
@@ -11,122 +18,112 @@
  * Group : IO-52
  * Task : MA = MB * min(E * MM) + MO * (MS * MX) * d
  */
+const static int COUNT = 10;
+
+void fillMatrix(int MA[N][N], int value);
+
+void fillVector(int A[N], int value);
+
+void printMatrix(int MA[N][N], int begin, int end);
 
 int main(int argc, char **argv) {
-memcpy()
-//    const rlim_t kStackSize = 256 * 1024 * 1024;   // min stack size = 256 MB
-//    struct rlimit rl;
-//    int result;
-//    result = getrlimit(RLIMIT_STACK, &rl);
-//    if (result == 0) {
-//        if (rl.rlim_cur < kStackSize) {
-//            rl.rlim_cur = kStackSize;
-//            result = setrlimit(RLIMIT_STACK, &rl);
-//            if (result != 0) {
-//                fprintf(s btderr, "setrlimit returned result = %d\n", result);
-//            }
-//        }
-//    }
-//    cout << "result = " << result << endl;
-    int name_len, size, rank;
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    double x[10];
 
+    int MS[N][N], MM[N][N], MB[N][N], MO[N][N], MX[N][N];
+    int E[N];
+    int d;
+
+    int size, rank;
     MPI_Status status;
     MPI_Init(NULL, NULL);    // Initialize the MPI environment
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     double half = (double) size / 2.0;
-    int a = 5;
-    Matrix MB, MO, MX;
-    Matrix MM;
-    Vector E;
-    int d;
-    int MS[N][N];
-    int *data = (int *) malloc(N * N * sizeof(int));
-    int **array = (int **) malloc(N * sizeof(int *));
 
-    if (((rank + 1) < (int) half) || (((rank + 1) == (int) half) && ((int) half != half))) {
-
+//    int a = 5;
+//    if (((rank + 1) < (int) half) || (((rank + 1) == (int) half) && ((int) half != half))) {
+    if (rank < (P / 2)) {
         if (rank == 0) {
-            cout << "process #" << rank << endl;
-//            MS.fillBy(1);
-            MM.fillBy(1);
-//            E.fillBy(1);
-//            d = 1;
-//            MS.print();
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    MS[i][j] = 1;
-                }
-                cout << *MS[i] << " ";
+//        cout << "process #" << rank << endl;
+
+            fillMatrix(MS, 1);
+            fillMatrix(MM, 1);
+            fillVector(E, 1);
+            d = 1;
+
+            for (int i = N / 2; i < N; i++) {
+                MPI_Send(&MS[i], N / 2, MPI_INT, (size - 1), (size - 1), MPI_COMM_WORLD);
+                MPI_Send(&MM[i], N / 2, MPI_INT, (size - 1), (size - 1), MPI_COMM_WORLD);
             }
-//            for (int i = 0; i < N; i++) {
+            MPI_Send(E, N, MPI_INT, (size - 1), (size - 1), MPI_COMM_WORLD);
+            MPI_Send(&d, 1, MPI_INT, (size - 1), (size - 1), MPI_COMM_WORLD);
 
-
-            for (int i = 0; i < N; i++)
-                array[i] = &(data[N * i]);
-
-            MPI_Send(&(array[0][0]), N * N, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
-//            }
-
-            for (int i = 0; i < 9; i++) {
-                x[i] = 0.1 * i;
+            for (int i = (rank + 1) * H; i < N; i++) {
+                MPI_Send(&MS[i], N - (rank + 1) * H, MPI_INT, rank + 1, (rank + 1), MPI_COMM_WORLD);
+                MPI_Send(&MM[i], N - (rank + 1) * H, MPI_INT, rank + 1, (rank + 1), MPI_COMM_WORLD);
             }
-//            printf("Hello world from rank %d, out of %d processors\n", rank, size);
+            MPI_Send(&E, N, MPI_INT, rank + 1, (rank + 1), MPI_COMM_WORLD);
+            MPI_Send(&d, 1, MPI_INT, (size - 1), (size - 1), MPI_COMM_WORLD);
 
-        } else {
-//            MPI_Recv(x, 9, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
-//            printf("Hello world from rank %d, out of %d processors\n", rank, size);
-//            MPI_Send((x + rank + 1), 9 - rank, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
-//            if (rank == 3) {
-//                for (int i = 0; i < 9; i++)
-//                    cout << x[i] << " ";
-//                cout << endl;
-//                cout << endl;
-//            }
+//        cout << "finished process #" << rank << endl;
         }
+    } else if (rank > P / 2) {
+        if (rank == (size - 1)) {
+//        cout << "process #" << rank << endl;
 
+            fillMatrix(MB, 1);
+            fillMatrix(MO, 1);
+            fillMatrix(MX, 1);
 
-    } else if ((rank - 2) > (int) half) {
+            for (int i = N / 2; i < N; i++) {
+                MPI_Recv(&MS[i], N / 2, MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
+                MPI_Recv(&MM[i], N / 2, MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
+            }
+            MPI_Recv(E, N, MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
+            MPI_Recv(&d, 1, MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
 
-        if (rank == size - 1) {
-            cout << "process #" << rank << endl;
-            MPI_Recv(&(array[0][0]), N * N, MPI_INT, 0, 666, MPI_COMM_WORLD, &status);
+//        cout << "H = " << H << "  N = " << N << endl;
+
+//        printMatrix(MS, 0, N);
+//        cout << "finished process #" << rank << endl;
         } else {
+//        cout << "process #" << rank << endl;
+
+            for (int i = rank * H; i < N; i++) {
+                MPI_Recv(&MS[i], N - (rank) * H, MPI_INT, (rank - 1), (rank), MPI_COMM_WORLD, &status);
+            }
+//        printMatrix(MS, rank * H, N);
+            for (int i = (rank + 1) * H; i < N; i++) {
+                MPI_Send(&MS[i], N - (rank + 1) * H, MPI_INT, rank + 1, (rank + 1), MPI_COMM_WORLD);
+            }
+//        cout << "finished process #" << rank << endl;
 
         }
-
     }
-
-//    if (rank == 0) {
-//        for (int i = 0; i < 10; i++)
-//            x[i] = 0.1 * i;
-//        MPI_Send(x, 5, MPI_DOUBLE, 1, 666, MPI_COMM_WORLD);
-//    } else if (rank == 1) {
-//        MPI_Recv(x, 5, MPI_DOUBLE, 0, 666, MPI_COMM_WORLD, &status);
-//    }
-//
-//    if (rank == 0) {
-//
-//        for (int i = 0; i < 10; i++)
-//            cout << x[i] << " ";
-//        cout << endl;
-//        cout << endl;
-//    } else {
-//        for (int i = 0; i < 10; i++)
-//            cout << x[i] << " ";
-//        cout << endl;
-//        cout << endl;
-//    }
-//
-////    MPI_Get_processor_name(processor_name, &name_len);
-////
-////    printf("Hello world from processor %s, rank %d"
-////           " out of %d processors\n",
-////           processor_name, rank, size);
-//
     MPI_Finalize();
-
 }
+
+void fillMatrix(int MA[N][N], int value) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            MA[i][j] = value;
+        }
+    }
+}
+
+void fillVector(int A[N], int value) {
+    for (int i = 0; i < N; i++) {
+        A[i] = value;
+    }
+}
+
+void printMatrix(int MA[N][N], int begin, int end) {
+    if (N <= COUNT) {
+        for (int i = begin; i < end; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << MA[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
